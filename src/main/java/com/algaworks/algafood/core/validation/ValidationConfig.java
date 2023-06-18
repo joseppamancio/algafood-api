e8e899c482383989ed7225590ef117baa31956eb
joseppamancio <jose_paulo_sjc@hotmail.com>
2023-06-18 10:56:04 -0300

    Conclusão do Módulo 10

--- src/main/java/com/algaworks/algafood/core/validation/ValidationConfig.java
+++ src/main/java/com/algaworks/algafood/core/validation/ValidationConfig.java
@@ -1,23 +1,23 @@
-//package com.algaworks.algafood.core.validation;
-//
-//import org.springframework.context.MessageSource;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-//
-//@Configuration
-//public class ValidationConfig {
-//	
-//	/**
-//	 * Define o ValidationSource com o MessageSoruce como Principal
-//	 * @param messageSource
-//	 * @return
-//	 */
-//	@Bean
-//	public LocalValidatorFactoryBean validator(MessageSource messageSource) {
-//		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-//		bean.setValidationMessageSource(messageSource);
-//		return bean;
-//	}
-//
-//}
+package com.algaworks.algafood.core.validation;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ValidationConfig {
+	
+	/**
+	 * Define o ValidationSource com o MessageSoruce como Principal
+	 * @param messageSource
+	 * @return
+	 */
+	@Bean
+	public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource);
+		return bean;
+	}
+
+}
