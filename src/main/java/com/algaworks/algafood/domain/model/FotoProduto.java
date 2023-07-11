76ccd8ad2abe20a6fea848688990fdfc7fac5d5f
joseppamancio <jose_paulo_sjc@hotmail.com>
2023-07-11 18:48:06 -0300

    Módulo 14 Finalizado

--- src/main/java/com/algaworks/algafood/domain/model/FotoProduto.java
+++ src/main/java/com/algaworks/algafood/domain/model/FotoProduto.java
@@ -1,37 +1,49 @@
-//package com.algaworks.algafood.domain.model;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.OneToMany;
-//
-//import lombok.Data;
-//import lombok.EqualsAndHashCode;
-//
-//@Data
-//@Entity
-//@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-//public class FotoProduto {
-//
-//	@Id
-//	@EqualsAndHashCode.Include
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private Long id;
-//	
-//	@Column(nullable = false)
-//	private String nomeArquivo;
-//	
-//	@Column(nullable = false)
-//	private String descricao;
-//	
-//	@Column(nullable = false)
-//	private String contentType;
-//	
-//	@Column(nullable = false)
-//	private Long tamanho;
-//	
-//	@OneToMany(mappedBy = "produto")
-//	private Produto produto;
-//}
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class FotoProduto {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@Column(name = "produto_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nomeArquivo;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private String contentType;
+	
+	@Column(nullable = false)
+	private Long tamanho;
+	
+	@MapsId
+	@OneToOne(fetch = FetchType.LAZY)
+	private Produto produto;
+	
+	
+	public Long getRestauranteId() {
+		if(getProduto() != null) {
+			return getProduto().getRestaurante().getId();
+		}
+		return null;
+	}
+}
