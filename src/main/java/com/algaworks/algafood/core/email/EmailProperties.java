9be19115b219bb176938734b5f4ad69717577597
joseppamancio <jose_paulo_sjc@hotmail.com>
2023-07-15 15:59:37 -0300

    Modulo 15 finalizado

---
+++ src/main/java/com/algaworks/algafood/core/email/EmailProperties.java
@@ -0,0 +1,43 @@
+package com.algaworks.algafood.core.email;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Validated
+@Component
+@ConfigurationProperties("algafood.email")
+public class EmailProperties {
+	
+	@NotNull
+	private String remetente;
+	
+	//Isso evita o problema de enviar e-mails de verdade caso você esqueça
+	private Implementacao impl = Implementacao.FAKE;
+	
+	private Sandbox sandbox = new Sandbox();
+
+	public enum Implementacao {
+	    SMTP, FAKE, SANDBOX
+	}
+	
+	@Getter
+	@Setter
+	public class Sandbox {
+	    
+	    private String destinatario;
+	    
+	}  
+}
+
+
+
+
+
--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -6,23 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.repository.PedidoRepository;
 
 @Service
 public class FluxoPedidoService {
 	
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
+		
+		pedidoRepository.save(pedido);
 	}
 	
 	@Transactional
 	public void cancelar(String codigoPedido) {
 	    Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 	    pedido.cancelar();
+	    
+	    pedidoRepository.save(pedido);
 	}
 	
 	@Transactional
