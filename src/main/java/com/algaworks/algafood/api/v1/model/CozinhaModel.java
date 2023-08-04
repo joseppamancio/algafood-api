73e8ef1b17f0360853191cb7f3a521c5c5f25c2a
joseppamancio <jose_paulo_sjc@hotmail.com>
2023-08-04 08:45:45 -0300

    Módulo 20 Finalizado

---
+++ src/main/java/com/algaworks/algafood/api/v1/model/CozinhaModel.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.api.v1.model;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Relation(collectionRelation = "cozinhas")
+public class CozinhaModel extends RepresentationModel<CozinhaModel>{
+	
+	@ApiModelProperty(example = "1")
+	private Long id;
+	
+	@ApiModelProperty(example = "Brasileira")
+	private String nome;
+
+}
---
+++ src/main/java/com/algaworks/algafood/api/v2/model/input/CozinhaInputV2.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.api.v2.model.input;
+import javax.validation.constraints.NotBlank;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@ApiModel("CozinhaInput")
+public class CozinhaInputV2 {
+
+    @ApiModelProperty(example = "Brasileira", required = true)
+    @NotBlank
+    private String nomeCozinha;   
+}        
