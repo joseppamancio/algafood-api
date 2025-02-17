650ab9166f505a3c68ae80dc0f4b601ad839d058
joseppamancio <jose_paulo_sjc@hotmail.com>
2025-02-17 15:36:31 +0100

    feat: adding openApi problem representation model.

--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -1,6 +1,10 @@
 package com.algaworks.algafood.core.springdoc;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
@@ -10,10 +14,15 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springdoc.core.GroupedOpenApi;
 import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @SecurityScheme(
@@ -45,10 +54,13 @@ public class SpringDocConfig {
                             .externalDocs(new ExternalDocumentation()
                                     .description("AlgaWorks")
                                     .url("https://algaworks.com")
-                            );
+                            ).components(new Components().schemas(
+                                    gerarSchemas()
+                            ));
                 })
                 .build();
     }
+
 
     @Bean
     public GroupedOpenApi v2Api() {
@@ -66,29 +78,62 @@ public class SpringDocConfig {
                             .externalDocs(new ExternalDocumentation()
                                     .description("AlgaWorks")
                                     .url("https://algaworks.com")
-                            );
+                            )
+                            .tags(Arrays.asList(
+                                    new Tag().name("Cidades").description("Gerencia as cidades")
+                            )).components(new Components().schemas(
+                                    gerarSchemas()
+                            ));
                 })
                 .build();
     }
 
     @Bean
-    public OpenApiCustomiser openApiCustomiser(){
+    public OpenApiCustomiser openApiCustomiser() {
         return openApi -> {
-            openApi.getPaths() // itera entre os paths da nossa aplicacao
+            openApi.getPaths()
                     .values()
-                    .stream()
-                    .flatMap(pathItem -> pathItem.readOperations().stream())
-                    .forEach(operation -> {
-                        ApiResponses responses = operation.getResponses();
-
-                        ApiResponse apiResponseNaoEncontrado = new ApiResponse().description("Recurso não encontrado");
-                        ApiResponse apiResponseErroInterno = new ApiResponse().description("Erro interno no servidor");
-                        ApiResponse apiResponseSemRepresentacao = new ApiResponse().description("Recurso não possui uma representação aceita pelo consumidor");
-
-                        responses.addApiResponse("406", apiResponseNaoEncontrado);
-                        responses.addApiResponse("406", apiResponseSemRepresentacao);
-                        responses.addApiResponse("500", apiResponseErroInterno);
-                    });
+                    .forEach(pathItem -> pathItem.readOperationsMap()
+                            .forEach((httpMethod, operation) -> {
+                                ApiResponses responses = operation.getResponses();
+                                switch (httpMethod) {
+                                    case GET:
+                                        responses.addApiResponse("404", new ApiResponse().description("Recurso não encontrado"));
+                                        responses.addApiResponse("406", new ApiResponse()
+                                                .description("Recurso não possui representação que poderia ser aceita pelo consumidor"));
+                                        responses.addApiResponse("500", new ApiResponse().description("Erro interno no servidor"));
+                                        break;
+                                    case POST:
+                                        responses.addApiResponse("400", new ApiResponse().description("Requisição inválida"));
+                                        responses.addApiResponse("500", new ApiResponse().description("Erro interno no servidor"));
+                                        break;
+                                    case PUT:
+                                        responses.addApiResponse("404", new ApiResponse().description("Recurso não encontrado"));
+                                        responses.addApiResponse("400", new ApiResponse().description("Requisição inválida"));
+                                        responses.addApiResponse("500", new ApiResponse().description("Erro interno no servidor"));
+                                        break;
+                                    case DELETE:
+                                        responses.addApiResponse("404", new ApiResponse().description("Recurso não encontrado"));
+                                        responses.addApiResponse("500", new ApiResponse().description("Erro interno no servidor"));
+                                        break;
+                                    default:
+                                        responses.addApiResponse("500", new ApiResponse().description("Erro interno no servidor"));
+                                        break;
+                                }
+                            })
+                    );
         };
+    }
+
+    private Map<String, Schema> gerarSchemas() {
+        final Map<String, Schema> schemaMap = new HashMap<>();
+
+        Map<String, Schema> problemSchema = ModelConverters.getInstance().read(Problem.class);
+        Map<String, Schema> problemObjectSchema = ModelConverters.getInstance().read(Problem.Object.class);
+
+        schemaMap.putAll(problemSchema);
+        schemaMap.putAll(problemObjectSchema);
+
+        return schemaMap;
     }
 }
