74bbf9f1498220d021ce0bd40e18db2d1e25f623
joseppamancio <jose_paulo_sjc@hotmail.com>
2025-02-22 20:32:03 +0100

    feat: opaque token

---
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class ResourceServerConfig {
+
+    @Bean
+    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/oauth/**").authenticated()
+                .and()
+                .csrf().disable()
+                .cors().and()
+                .oauth2ResourceServer().opaqueToken();
+
+        return http.build();
+    }
+}
